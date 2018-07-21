0ef5d9a7c3bbffd310f6fc30ba8e18252e135e5a
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-21 23:22:46 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter=0
-  loop do 
+  loop  
    puts phrase
    counter+=1
    if counter>= number_of_times
